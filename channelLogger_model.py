4cd7d3b002cfd2b6031ec61a38b6e5cfeb317761
Jason Williams <jase.williams@gmail.com>
2014-01-26 23:21:29 +0000

    first commit

---
+++ channelLogger_model.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+import sys
+import json
+import os
+import time
+import psycopg2
+
+class LogviewerDB:
+
+	def __init__(self):
+		# DB connection string
+		try:
+			with open('plugins/ChannelLogger/config.json') as data:
+				config = json.load(data)
+
+			conn_string = "host='%s' dbname='%s' user='%s' password='%s'" % (config['db']['host'], config['db']['dbname'], config['db']['user'], config['db']['password'])
+		except IOError as e:
+			print os.getcwd()
+			sys.exit("Error! No config file supplied, please create a config.json file in the root")
+		
+		# Print connection string
+		print "Connecting to database\n -> %s" % (conn_string)
+		
+		# get a connection
+		conn = psycopg2.connect(conn_string)
+		
+		# conn.curser will return a cursor object, you can use this to perform queries
+		self.cursor = conn.cursor()
+		self.conn = conn
+		print "connected!\n"
+
+	def add_message(self, user, host, msg):
+		self.__add_message(user, host, msg, 'message')
+
+	def add_join(self, user, host):
+		self.__add_message(user, host, '', 'join')
+
+	def add_part(self, user, host):
+		self.__add_message(user, host, '', 'part')
+
+	def add_quit(self, user, host):
+		self.__add_message(user, host, '', 'quit')
+
+	def add_emote(self, user, host, msg,):
+		self.__add_message(user, host, msg, 'emote')
+
+	def __add_message(self, user, host, msg, action):
+		# Was this message from a user we already have in our database?
+		# If so return the userID.
+		userID = self.check_user_host_exists(user, host) or False
+		# If userID is False, store the new combo then get back the userID
+		if not userID:
+			self.cursor.execute("INSERT INTO users (\"user\", \"host\") VALUES (%s, %s)", (user, host))
+			self.conn.commit()
+			# We should now have an ID for our new user/host combo
+			userID = self.check_user_host_exists(user, host);
+
+		if (action == 'message' or action == 'emote'):	
+			self.cursor.execute("INSERT INTO messages (\"user\", \"content\", \"action\") VALUES (%s, %s, %s)", (userID, msg, action))
+		else:
+			self.cursor.execute("INSERT INTO messages (\"user\", \"action\") VALUES (%s, %s)", (userID, action))
+		self.conn.commit()
+
+	# Check if user exists then return the user ID, if not return false
+	def check_user_host_exists(self, user, host):
+		self.cursor.execute("SELECT * FROM users WHERE \"user\"= %s AND host=%s", (user, host))
+		if self.cursor.rowcount:
+			return self.cursor.fetchone()[1]
+		else:
+			return False
+
+	def resetData(self):
+		self.cursor.execute("DELETE FROM users;")
+		self.cursor.execute("DELETE FROM messages;")
+		self.cursor.execute("ALTER SEQUENCE messages_id_seq RESTART WITH 1;")
+		self.cursor.execute("ALTER SEQUENCE users_id_seq RESTART WITH 1;")
+		self.conn.commit()
+
+class LogviewerFile:
+
+	def __init__(self):
+		# Get Logfile Path
+		try:
+			with open('plugins/ChannelLogger/config.json') as data:
+				config = json.load(data)
+
+			self.logPath = config['logs']['folderPaths']
+		except IOError as e:
+			sys.exit("Error! No config file supplied, please create a config.json file in the root")
+
+	def write_message(self, user, msg):
+		time_stamp = time.strftime("%H:%M:%S")
+		dateStamp = time.strftime("%Y-%m-%d")
+		with open(self.logPath + "/%s.log" % dateStamp, 'a') as logFile:
+			logFile.write("%s <%s> %s" % (time_stamp, user, msg))
+
+	def write_join(self, user, host):
+		time_stamp = time.strftime("%H:%M:%S")
+		dateStamp = time.strftime("%Y-%m-%d")
+		with open(self.logPath + "/%s.log" % dateStamp, 'a') as logFile:
+			logFile.write("%s --> <%s> (%s) joins %s \n" % (time_stamp, user, host, channel))
+
+	def write_part(self, user, host):
+		time_stamp = time.strftime("%H:%M:%S")
+		dateStamp = time.strftime("%Y-%m-%d")
+		with open(self.logPath + "/%s.log" % dateStamp, 'a') as logFile:
+			logFile.write("%s <-- <%s> (%s) parts %s \n" % (time_stamp, user, host, channel))
+
+	def write_quit(self, user, host):
+		time_stamp = time.strftime("%H:%M:%S")
+		dateStamp = time.strftime("%Y-%m-%d")
+		with open(self.logPath + "/%s.log" % dateStamp, 'a') as logFile:
+			logFile.write("%s <-- <%s> (%s) quits %s \n" % (time_stamp, user, host, channel))
+
+
+def main():
+	logviewerDB = LogviewerDB()
+	logviewerDB.resetData();
+
+if __name__ == "__main__":
+	main()
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import subprocess
+import os
+import getpass
+import sys
+import json
+# Postgresql needs to be install
+# libpq-dev and python-dev needs to be installed for psycopg2
+if not os.path.exists('config.json'):
+	print "First a config file needs to be created......"
+	userName = raw_input('Please enter your user name (pref your OS username): ')
+	userPass = getpass.getpass('Please enter your password for the Postgresql database (won\'t echo)');
+	with open('config.json', 'w') as configFile:
+		configText = """
+{
+	"db": {
+		"host": "127.0.0.1",
+		"dbname": "logs_stats",
+		"user": "%s",
+		"password": "%s"
+	}
+}
+	""" % (userName, userPass, userName)
+		configFile.write(configText)
+	confirm = raw_input("Are you running this script from the host machine? yes/no: ")
+	if (confirm == "yes"):
+		print "Run again from guest"
+		sys.exit();
+
+# open the config file
+with open('config.json', 'r') as configFile:
+	data = json.loads(configFile.read());
+
+userName = data['db']['user']
+userPass = data['db']['password']
+
+subprocess.call(['apt-get', 'install', '-y', 'postgresql', 'libpq-dev', 'python-dev', 'python-pip'])
+subprocess.call(['pip', 'install', 'psycopg2'])
+
+os.system('echo "CREATE ROLE %s LOGIN ENCRYPTED PASSWORD \'%s\';" | sudo -u postgres psql' % (userName, userPass))
+os.system('echo "CREATE DATABASE logs_stats OWNER %s;" | sudo -u postgres psql' % userName)
+os.system('sudo -u postgres psql logs_stats < logs_stats.sql')
+
+os.system('echo "ALTER TABLE messages OWNER TO %s;" | sudo -u postgres psql logs_stats' % userName)
+os.system('echo "ALTER TABLE messages_id_seq OWNER TO %s;" | sudo -u postgres psql logs_stats' % userName)
+os.system('echo "ALTER TYPE action OWNER TO %s;" | sudo -u postgres psql logs_stats' % userName)
+os.system('echo "ALTER TABLE users OWNER TO %s;" | sudo -u postgres psql logs_stats' % userName)
+os.system('echo "ALTER TABLE users_id_seq OWNER TO %s;" | sudo -u postgres psql logs_stats' % userName)
